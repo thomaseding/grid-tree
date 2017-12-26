bb94178234bdbd190d1db4f131e20d19e8e2db1d
Thomas Eding <thomaseding@gmail.com>
2017-12-26 09:54:39 -0800

    wip

--- src/Data/GridTree/Text.hs
+++ src/Data/GridTree/Text.hs
@@ -19,17 +19,24 @@ import Data.Text.Prettyprint.Doc
 --------------------------------------------------------------------------------
 
 
-framedText :: Grid 'Absolute -> Doc ann -> (SimpleDocStream ann -> Text) -> Text
-framedText frame doc renderer = let
+-- XXX:
+-- Perhaps a better way of doing all this is to handle the console offsets as annotations.
+-- (Perhaps also the frame cutoffs should also be left for the renderer?)
+-- Then the renderer can do all the offsetting (in addition to other ANSI stuff).
+-- This is probably MUCH easier to implement and does what I need in the end anyway.
+
+
+framedText :: Grid 'Absolute -> Doc ann -> SimpleDocStream ann
+framedText frame doc = let
     (totalWidth, totalHeight) = dimensionsOf frame
     layoutOptions = LayoutOptions {
         layoutPageWidth = AvailablePerLine totalWidth 1 }
     docStream = layoutSmart layoutOptions doc
-    docStream' = confine 0 0 docStream
+    docStream' = shiftVertical $ shiftHorizontal $ confine 0 0 docStream
     --
     confine !currWidth !currHeight = \case
-        SFail -> SFail
-        SEmpty -> SEmpty
+        x @ SFail -> x
+        x @ SEmpty -> x
         SChar c rest -> case currWidth == totalWidth of
             False -> SChar c $ confine (currWidth + 1) currHeight rest
             True -> confine currWidth currHeight rest
@@ -53,11 +60,63 @@ framedText frame doc renderer = let
         SAnnPush ann rest -> SAnnPush ann $ confine currWidth currHeight rest
         SAnnPop rest -> SAnnPop $ confine currWidth currHeight rest
     --
-    in renderer docStream'
+    -- TODO:
+    -- May need to do something special for the initial line.
+    -- This might be something like injecting
+    --  (SLine totalWidth rest)
+    --  or
+    --  (SText totalWidth spaces rest)
+    shiftHorizontal = \case
+        x @ SFail -> x
+        x @ SEmpty -> x
+        SChar c rest -> SChar c $ shiftHorzintal rest
+        SText !textLength text rest -> SText textLength text $ shiftHorizontal rest
+        SLine !indentLevel rest -> SLine (indentLevel + totalWidth) $ shiftHorizontal rest
+        SAnnPush ann rest -> SAnnPush ann $ shiftHorizontal rest
+        SAnnPop rest -> shiftHorizontal rest
+    --
+    shiftVertical = shiftVertical' totalHeight
+    shiftVertical' rest = \case
+        0 -> rest
+        n -> SLine 0 $ shiftVertical' (n - 1) rest
+    --
+    in docStream'
 
 
-multiFramedText :: CutResult (Doc ann) -> (SimpleDocStream ann -> Text) -> Text
-multiFramedText = undefined
+zipStreamsMany :: [SimpleDocStream ann] -> SimpleDocStream ann
+zipStreamsMany = xxx
+
+
+tightenStream :: SimpleDocStream ann -> SimpleDocStream ann
+tightenStream = \case
+    x @ SFail -> x
+    x @ SEmpty -> x
+    SChar c rest -> tightenStream $ SText 1 (singleton c) rest
+    SText !l1 t1 (SText !l2 t2 rest) = let
+        l3 = l1 + l2
+        t3 = t1 `append` t2
+        in tightenStream l3 t3 rest
+    SLine !indentLevel rest -> SLine indentLevel $ tightenStream rest
+    SAnnPush ann -> SAnnPush ann $ tightenStream rest
+    SAnnPop -> SAnnPop $ tightenStream rest
+
+
+zipStreams :: SimpleDocStream ann -> SimpleDocStreamAnn -> SimpleDocStream ann
+zipStreams = zipStreams' `on` tightenStream
+
+
+zipStreams' :: SimpleDocStream ann -> SimpleDocStreamAnn -> SimpleDocStream ann
+zipStreams' SFail _ = SFail
+zipStreams' _ SFail = SFail
+zipStreams' SEmpty _ = SEmpty
+zipStreams' _ SEmpty = SEmpty
+zipStreams' SChar{} _ = error "Internal logic error."
+zipStreams' _ SChar{} = error "Internal logic error."
+zipStreams' = undefined -- Some combination of Text.take and Text.append. Complicated by annotations.
+
+
+multiFramedText :: CutResult (Doc ann) -> SimpleDocStream ann
+multiFramedText = undefined -- Walk tree to obtiain streams and then zip them.
 
 
 
